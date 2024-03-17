c6cdf17bdb2e6805b9a970227a6cf30248ca5910
DESKTOP-3HAS177\rusni <rus.nigmat@gmail.com>
2024-03-17 17:11:06 +0700

    Init commit

---
+++ src/main/java/ru/inno/educ/springfilereadwrite/SpringFileReadWriteApplication.java
@@ -0,0 +1,13 @@
+package ru.inno.educ.springfilereadwrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileReadWriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFileReadWriteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/inno/educ/springfilereadwrite/SpringFileReadWriteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.inno.educ.springfilereadwrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFileReadWriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
